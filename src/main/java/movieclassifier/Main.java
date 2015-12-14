8a8da2cc02b340f09c81ec044efe2219d21eebc9
zlizah <zleazor@gmail.com>
2015-12-14 17:52:20 +0100

    Added maven structure

---
+++ src/main/java/movieclassifier/Main.java
@@ -0,0 +1,10 @@
+package movieclassifier;
+
+/**
+ * Hello world!
+ */
+public class Main  {
+    public static void main( String[] args ) {
+        System.out.println("Hello Movie Classifying!");
+    }
+}
